d70e809edeaff950409241721032273a99f92db9
김영현 <kim-younghyeon@gim-yeonghyeon-ui-MacBookAir.local>
2023-11-18 16:31:11 +0900

    part3 class2

---
+++ vuestagram/src/components/ContainerBox.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <PostBox v-for="(item, index) in Posting" :key="index" :Posting="item" />
+  </div>
+</template>
+
+<script>
+import PostBox from './PostBox.vue';
+
+export default {
+    name : "ContainerBox",
+    components : {
+      PostBox,
+    },
+    props: {
+      Posting : Array,
+    },
+}
+</script>
+
+<style>
+
+</style>
